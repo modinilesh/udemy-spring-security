f551e262564d3f8fe1baafc8846c72896c50b61e
Nilesh_Modi <nilesh.modi.nitb@gmail.com>
2024-07-02 14:38:37 +0530

    Using Approach 1 : User.withDefaultPasswordEncoder() for creating InMemoryUserDetailsManager. Users created.

--- spring-security/spring-security/src/main/java/com/modinilesh/spring_security/config/BankingSecurityConfiguration.java
+++ spring-security/spring-security/src/main/java/com/modinilesh/spring_security/config/BankingSecurityConfiguration.java
@@ -4,12 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class BankingSecurityConfiguration {
+
+    //Approach 1 is mentioned below which uses withDefaultPasswordEncoder, but it is not recommended approach to use in production as it does not encode the password
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -19,6 +24,23 @@ public class BankingSecurityConfiguration {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin@123")
+                .authorities("admin")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("nilesh")
+                .password("Nilesh@123")
+                .authorities("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
