575b8b2ee0914ab19ed08e01c9cc4592d6fe2806
Nilesh_Modi <nilesh.modi.nitb@gmail.com>
2024-07-04 14:56:36 +0530

    Using Approach 2 : Using User.withUsername("") method which uses the NoOpPasswordEncoder, which simply stores the passwords as plain text. This approach is also not recommended for production

--- spring-security/spring-security/src/main/java/com/modinilesh/spring_security/config/BankingSecurityConfiguration.java
+++ spring-security/spring-security/src/main/java/com/modinilesh/spring_security/config/BankingSecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,33 +16,56 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class BankingSecurityConfiguration {
 
-    //Approach 1 is mentioned below which uses withDefaultPasswordEncoder, but it is not recommended approach to use in production as it does not encode the password
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("*/myAccount", "*/myBalance", "*/myCards", "*6/myLoans").authenticated()
-                .requestMatchers("*/contact", "*/notices").permitAll())
+                        .requestMatchers("*/myAccount", "*/myBalance", "*/myCards", "*/myLoans").authenticated()
+                        .requestMatchers("*/contact", "*/notices").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 
+    //Approach 1 is mentioned below which uses withDefaultPasswordEncoder, but it is not recommended approach to use in production as it does not encode the password
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails user1 = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("admin@123")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user2 = User.withDefaultPasswordEncoder()
+//                .username("nilesh")
+//                .password("Nilesh@123")
+//                .authorities("admin")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+
+    //Approach 2 : Using User.withUsername("") method which uses the NoOpPasswordEncoder, which simply stores the passwords as plain text. This approach is also not recommended for production
     @Bean
     public InMemoryUserDetailsManager userDetailsService(){
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails user1 = User.withUsername("admin")
                 .password("admin@123")
                 .authorities("admin")
                 .build();
 
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("nilesh")
+        UserDetails user2 = User.withUsername("nilesh")
                 .password("Nilesh@123")
                 .authorities("admin")
                 .build();
 
         return new InMemoryUserDetailsManager(user1, user2);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
 }
